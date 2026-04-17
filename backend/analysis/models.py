15923c74b6d65261000d02e7ec8afc26d1cdf0b0
Anijesh <anijeshkr@gmail.com>
2026-04-17 16:45:18 +0530

    Add initial migration and model definition for Analysis entity

--- backend/analysis/models.py
+++ backend/analysis/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Analysis(models.Model):
+    LANGUAGE_CHOICE =[
+        ('Python3', 'Python3'),
+        ('Python', 'Python'),
+        ('JavaScript', 'JavaScript'),
+        ('TypeScript', 'TypeScript'),
+        ('Java', 'Java'),
+        ('C++', 'C++'),
+        ('C', 'C'),
+        ('C#', 'C#'),
+        ('Go', 'Go'),
+        ('Rust', 'Rust'),
+        ('Kotlin', 'Kotlin'),
+        ('Swift', 'Swift'),
+        ('Ruby', 'Ruby'),
+        ('Scala', 'Scala'),
+        ('PHP', 'PHP'),
+        ('Dart', 'Dart'),
+        ('Elixir', 'Elixir'),
+        ('Erlang','Erlang'),
+        ('Racket', 'Racket')
+    ]
+    problem_name = models.CharField(max_length = 500)
+    language = models.CharField(max_length=30, choices=LANGUAGE_CHOICE)
+    problem_description = models.TextField(blank=True, null=True)
+    code = models.TextField(blank = False, null = False)
+    result = models.JSONField(null=True, blank=True)
+    cache_hit = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.problem_name} ({self.language})"
