7fe1a0434b86798560374a4575a08e366cb6c97e
Anijesh <anijeshkr@gmail.com>
2026-04-29 01:44:01 +0530

    Add URL patterns for history and analysis detail views

--- backend/analysis/urls.py
+++ backend/analysis/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import AnalyzeView
+from .views import AnalyzeView,HistoryView,AnalysisDetailView
 
 urlpatterns = [
     path('analyze/', AnalyzeView.as_view(), name='analyze'),
+    path('history/', HistoryView.as_view(), name='history'),
+    path('analyze/<int:pk>/', AnalysisDetailView.as_view(), name='analyze-detail'),
 ]
